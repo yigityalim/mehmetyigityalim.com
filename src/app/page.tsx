1baee5715c6a8515293efe9b21fcc69b256512a6
yigit <mehmetyigityalim@gmail.com>
2023-11-04 11:58:22 +0300

    vercel için debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,23 +47,6 @@ export default async function Home(): Promise<JSX.Element> {
                         className='rounded-full'
                     />
                     <h1 className='text-4xl font-bold tracking-tight text-gray-900 dark:text-white'>{title}</h1>
-                    <ul className='flex w-full flex-col items-center justify-center gap-y-4'>
-                        {SocialMedia.map(({ icon, title, url }, index) => (
-                            <li key={title} className='h-full w-full'>
-                                <a
-                                    href={url}
-                                    className='inline-flex w-full items-center justify-center gap-x-4 rounded bg-gray-50 p-3 text-2xl text-gray-900 transition duration-150 ease-in-out active:scale-90 dark:bg-[#111] dark:text-white'
-                                    title={title}
-                                    target='_blank'
-                                    rel='noreferrer noopener'
-                                    role='button'
-                                    tabIndex={index}
-                                >
-                                    {icon} {title}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
                 </Suspense>
             ))}
         </Container>
