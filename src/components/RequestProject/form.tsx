5c8249c2db89f849adb279d826429d9c5f4e0a40
yigityalim <mehmetyalm@icloud.com>
2023-11-29 22:53:42 +0300

    düzeltme

--- src/components/RequestProject/form.tsx
+++ src/components/RequestProject/form.tsx
@@ -1,29 +1,12 @@
 import React from 'react'
+import sendEmail from 'components/RequestProject/sendEmail'
 
 export default function ResendForm() {
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        fetch('/api/send', {
-            method: 'POST',
-            body: JSON.stringify({
-                name: 'test',
-                email: 'test@gmail.com',
-                message: 'message',
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((res) => res.json())
-            .then(console.log)
-            .catch(console.error)
-    }
-
     return (
-        <form onSubmit={onSubmit}>
-            <input name='name' placeholder='Name' />
-            <input name='email' placeholder='Email' />
-            <input name='message' placeholder='Message' />
+        <form action={async (formData) => await sendEmail(formData)}>
+            <input name='from' placeholder='Email' />
+            <input name='subject' placeholder='Subject' />
+            <textarea name='text' placeholder='Message' />
             <button type='submit'>Submit</button>
         </form>
     )
---
+++ src/lib/resend.ts
@@ -0,0 +1,2 @@
+import { Resend } from 'resend'
+export default new Resend(process.env.RESEND_API_KEY)
