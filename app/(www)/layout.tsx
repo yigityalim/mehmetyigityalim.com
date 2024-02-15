a85ef7506165d3090a850455b36ffca6543026f9
yigityalim <mehmetyalm@icloud.com>
2024-02-16 00:26:38 +0300

    vercel için güncelleme 3

--- app/(www)/layout.tsx
+++ app/(www)/layout.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import React from 'react'
 import { Analytics } from 'components/Analytics'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import type { Metadata, Viewport } from 'next'
+import type { Metadata } from 'next'
 import { fontSans } from 'lib/fonts'
 import { ClientSideProvider } from '@/components/Providers/clientSideProvider'
 import Intro from 'components/Intro'
@@ -22,13 +22,6 @@ export const metadata: Metadata = {
     },
     referrer: 'no-referrer',
 } as Metadata satisfies Metadata
-
-export const viewport: Viewport = {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
-} as Viewport satisfies Viewport
 
 type RootLayoutProps = Readonly<{ children: React.ReactNode }>
 
