056fd7fe13bdbeaaf0dd0e8c30dcead17427126b
yigityalim <mehmetyalm@icloud.com>
2024-03-04 10:50:20 +0300

    vercel hata düzeltmesi 2

--- lib/hooks/index.tsx
+++ lib/hooks/index.tsx
@@ -5,20 +5,18 @@ export const useIsIntro = (): boolean => {
     const [hasVisited, setHasVisited] = useState<boolean>(false)
 
     useEffect(() => {
-        const storedValue = sessionStorage.getItem('hasVisited')
-        if (storedValue) {
+        const storedValue = window.sessionStorage.getItem('hasVisited')
+        if (storedValue) setHasVisited(true)
+
+        const timeout: Timer = setTimeout(() => {
             setHasVisited(true)
+            window.sessionStorage.setItem('hasVisited', 'true')
+        }, 3100)
+
+        return (): void => {
+            clearTimeout(timeout)
         }
     }, [])
-
-    const timeout: Timer = setTimeout(() => {
-        setHasVisited(true)
-        sessionStorage.setItem('hasVisited', 'true')
-    }, 3100)
-
-    useEffect(() => {
-        return () => clearTimeout(timeout)
-    }, [timeout])
 
     return !hasVisited
 }
