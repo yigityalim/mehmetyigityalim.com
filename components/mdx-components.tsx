5976c20098f98e1028aaa735c3d9006d31837e0e
yigityalim <mehmetyalm@icloud.com>
2024-03-24 00:08:11 +0300

    eklentiler.

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -158,7 +158,7 @@ export const components = {
             {...props}
         />
     ),
-    Steps: ({ ...props }) => (
+    Steps: ({ ...props }: React.ComponentProps<'div'>) => (
         <div className='[&>h3]:step steps mb-12 ml-4 border-l pl-8 [counter-reset:step]' {...props} />
     ),
     Link: ({ className, ...props }: React.ComponentProps<typeof Link>) => (
